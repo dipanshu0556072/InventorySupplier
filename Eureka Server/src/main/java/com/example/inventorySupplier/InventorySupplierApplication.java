927bb7bb45d77ff3dab8f81e94d3c67d98b68490
dipanshu0556072 <dipanshukumar20aug@gmail.com>
2026-03-10 00:53:04 +0530

    initial push

---
+++ Eureka Server/src/main/java/com/example/inventorySupplier/InventorySupplierApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorySupplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySupplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySupplierApplication.class, args);
+	}
+
+}
---
+++ Eureka Server/src/test/java/com/example/inventorySupplier/InventorySupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventorySupplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
